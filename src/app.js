26fb96564e6e27485b96392b171efe9dab771b84
hpassen <passen@uchicago.edu>
2021-03-14 19:15:28 -0500

    some of the animation llama

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import 'intersection-observer';
 import scrollama from 'scrollama';
 import {json} from 'd3-fetch';
 import {select, selectAll} from 'd3-selection';
-import {scaleLinear, scaleBand, scaleOrdinal} from 'd3-scale';
+import {scaleLinear, scaleBand, scaleOrdinal, scaleThreshold} from 'd3-scale';
 import {extent} from 'd3-array';
 import {line} from 'd3-shape';
 import {axisLeft, axisBottom, tickFormat} from 'd3-axis';
@@ -61,7 +61,6 @@ var step = article.selectAll('.step');
 var myData = {};
 var defaults = {};
 // Data Constants
-const xCol = 'year';
 const xLabel = 'Year';
 const scrollConfig = {
   0: 1990,
@@ -104,9 +103,63 @@ const scrollConfig = {
 // Set up Chart Sizing Constants
 const width = 600;
 const height = (24 / 36) * width;
-const margin = {top: 60, bottom: 60, right: 30, left: 100};
+const margin = {top: 60, bottom: 80, right: 30, left: 100};
 const plotHeight = height - margin.top - margin.bottom;
 const plotWidth = width - margin.left - margin.right;
+
+//
+//
+//GET THE DATA AND RUN THE PAGE
+Promise.all(
+  [
+    './data/renewables.json',
+    './data/source.json',
+    // 'https://hpassen.github.io/energy_emissions_dynamic/data/renewables.json',
+    // 'https://hpassen.github.io/energy_emissions_dynamic/data/source.json',
+  ].map((url) => fetch(url).then((x) => x.json())),
+)
+  .then((results) => {
+    //Name the constants
+    const [renewables, source] = results;
+    myData = {renew: renewables, src: source};
+    console.log('...in the promise...myData:', myData);
+    defaults = {
+      xCol: 'year',
+      yCol: 'gen_mwh',
+      dataset: source,
+      filterVal: 'src',
+      colorScale: srcColors,
+      geog: 'United States',
+    };
+    console.log('in the promise...defaults:', defaults);
+
+    //Build the Axes/Chart Body
+    buildContainers('#scrollfig', xLabel);
+
+    // Generate the scroll and the interactive dropdowns
+    scroll();
+
+    buildContainers('#bar', xLabel);
+    testBar(source, '#bar', 1990);
+    buildContainers('#smalllines', xLabel);
+    testLines(source, '#smalllines');
+    uxDynamic(results, '#dynamic', defaults);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// //Build the Axes/Chart Body
+// buildContainers('#scrollfig', xLabel);
+
+// // Generate the scroll and the interactive dropdowns
+// scroll();
+
+// buildContainers('#bar', xLabel);
+// testBar(myData.src, '#bar', 1990);
+// buildContainers('#smalllines', xLabel);
+// testLines(myData.src, '#smalllines');
+// uxDynamic([myData.renew, myData.src], '#dynamic', defaults);
 
 //
 //
@@ -118,17 +171,30 @@ var scroller = scrollama();
 function handleResize() {
   // 1. update height of step elements
   // var stepH = Math.floor(window.innerHeight * 0.75);
-  // var stepH = Math.floor(window.innerHeight * 0.5);
+  var stepH = Math.floor(window.innerHeight * 0.75);
   step.style('height', height + 'px');
 
   var figureHeight = window.innerHeight / 2;
+  var figureWidth = figureHeight / (24 / 36);
   var figureMarginTop = (window.innerHeight - figureHeight) / 2;
 
+  var scaleH = figureHeight / height;
+  var scaleW = figureWidth / width;
+
+  console.log('...the figure height and width are', figureHeight, figureWidth);
   figure
     .style('height', figureHeight + 'px')
+    .style('width', figureWidth + 'px')
     .style('top', figureMarginTop + 'px');
 
-  chart.style('height', height + 'px').style('width', width + 'px');
+  console.log(
+    '... so the plot height should be',
+    plotHeight * scaleH,
+    plotWidth * scaleW,
+  );
+  chart
+    .attr('height', plotHeight * scaleH + 'px')
+    .attr('width', plotWidth * scaleW + 'px');
   // 3. tell scrollama to update new element dimensions
   scroller.resize();
 }
@@ -145,15 +211,25 @@ function handleStepEnter(response) {
 
   // update graphic based on step
   if (response.index < 2) {
+    if (response.direction == 'up' && response.index == 1) {
+      var scrollfig = selectAll('#scrollfig');
+      scrollfig.selectAll('.line').remove();
+    }
+
     renderBar(defaults.dataset, id, scrollConfig[response.index]);
+    // select the lines and if it's not an empty array, remove the lines
   } else if (1 < response.index && response.index < 9) {
+    if (response.direction == 'down' && response.index == 2) {
+      var scrollfig = selectAll('#scrollfig');
+      scrollfig.selectAll('rect').remove();
+    }
+    // select the bars and if not empty, remove them
+    var xCol = defaults.xCol;
     var yCol = defaults.yCol;
     var geog = defaults.geog;
     var dataset = defaults.dataset;
     var filterVal = defaults.filterVal;
     var colorScale = defaults.colorScale;
-
-    const t = transition().duration(1000);
     // const loc = filter_geog(dataset, 'United States');
     console.log('...testing... loc:', dataset);
 
@@ -161,8 +237,9 @@ function handleStepEnter(response) {
     var lineData = columnHas(dataset, 'src', filters);
     // console.log('...testing columnhas:', lineData);
 
-    renderLines(yCol, lineData, filterVal, colorScale, geog, id);
+    renderLines(xCol, yCol, lineData, filterVal, colorScale, geog, id);
   } else {
+    var xCol = defaults.xCol;
     var yCol = defaults.yCol;
     var geog = defaults.geog;
     var filterVal = 'renew';
@@ -176,8 +253,6 @@ function handleStepEnter(response) {
       colorScale,
       dataset,
     );
-
-    const t = transition().duration(1000);
     // const loc = filter_geog(dataset, 'United States');
     console.log('...testing... loc:', dataset);
 
@@ -185,8 +260,7 @@ function handleStepEnter(response) {
     console.log('...testing filters:', filters);
     var lineData = columnHas(dataset, 'renew', filters);
     console.log('...testing columnhas:', lineData);
-
-    renderLines(yCol, lineData, filterVal, colorScale, geog, id);
+    renderLines(xCol, yCol, lineData, filterVal, colorScale, geog, id);
   }
 }
 
@@ -223,51 +297,12 @@ scroll();
 
 //
 //
-//GET THE DATA AND RUN THE PAGE
-Promise.all(
-  [
-    './data/renewables.json',
-    './data/source.json',
-    // 'https://hpassen.github.io/energy_emissions_dynamic/data/renewables.json',
-    // 'https://hpassen.github.io/energy_emissions_dynamic/data/source.json',
-  ].map((url) => fetch(url).then((x) => x.json())),
-)
-  .then((results) => {
-    //Name the constants
-    const [renewables, source] = results;
-    myData = {renew: renewables, src: source};
-    console.log('...in the promise...myData:', myData);
-    defaults = {
-      yCol: 'gen_mwh',
-      dataset: source,
-      filterVal: 'src',
-      colorScale: srcColors,
-      geog: 'United States',
-    };
-    console.log('in the promise...defaults:', defaults);
-
-    //Build the Axes/Chart Body
-    buildContainers('#scrollfig', xLabel);
-
-    // Generate the scroll and the interactive dropdowns
-    scroll();
-    // buildContainers('#bar', xLabel);
-    // testBar(source, '#bar', 1990);
-    buildContainers('#smalllines', xLabel);
-    testLines(source, '#smalllines');
-    uxDynamic(results, '#dynamic', defaults);
-  })
-  .catch((e) => {
-    console.log(e);
-  });
-
-//
-//
 //TESTING THE FILTERED LINES
 function testLines(dataset, id) {
   console.log('...testing...data:', dataset);
   console.log('...testing...id:', id);
   // DEFAULTS
+  var xCol = defaults.xCol;
   var yCol = defaults.yCol;
   var geog = defaults.geog;
   var filterVal = defaults.filterVal;
@@ -281,7 +316,7 @@ function testLines(dataset, id) {
   var lineData = columnHas(loc, 'src', filters);
   console.log('...testing columnhas:', lineData);
 
-  renderLines(yCol, lineData, filterVal, colorScale, geog, id);
+  renderLines(xCol, yCol, lineData, filterVal, colorScale, geog, id);
 }
 
 //
@@ -342,14 +377,21 @@ function renderBar(dataset, id, year) {
         ),
     );
 
-  chart.select('.axisContainerX').call(axisBottom(xScale));
+  chart
+    .select('.axisContainerX')
+    .call(axisBottom(xScale).tickSize(0))
+    .selectAll('text')
+    .attr('x', -10)
+    .attr('y', 0)
+    .attr('transform', 'rotate(-60)')
+    .attr('text-anchor', 'end');
   chart.select('.axisContainerY').call(
     axisLeft(yScale.range([plotHeight, 0]))
       .ticks(5)
       .tickSizeOuter(0)
       .tickFormat(format('.2s')),
   );
-  xLab.select('text').text('Source');
+  xLab.select('text').text('Source').style('opacity', 0);
   // createLegend(barData, srcColors, legend);
 }
 
@@ -364,6 +406,7 @@ function uxDynamic(data, id, defaults) {
   console.log('in myVis the id is', id);
 
   // DEFAULTS:
+  var xCol = defaults.xCol;
   var yCol = defaults.yCol;
   var geog = defaults.geog;
   var dataset = defaults.dataset;
@@ -402,7 +445,7 @@ function uxDynamic(data, id, defaults) {
     .on('change', (event) => {
       var measure = event.target.value;
       yCol = measures[measure];
-      renderLines(yCol, dataset, filterVal, colorScale, geog, id);
+      renderLines(xCol, yCol, dataset, filterVal, colorScale, geog, id);
     })
     .selectAll('option')
     .data((dim) => measures_vars.map((measurement) => ({measurement, dim})))
@@ -426,7 +469,7 @@ function uxDynamic(data, id, defaults) {
     .append('select')
     .on('change', (event) => {
       [dataset, filterVal, colorScale] = datasets[event.target.value];
-      renderLines(yCol, dataset, filterVal, colorScale, geog, id);
+      renderLines(xCol, yCol, dataset, filterVal, colorScale, geog, id);
     })
     .selectAll('option')
     .data((dim) => datasets_vars.map((dataset) => ({dataset, dim})))
@@ -451,7 +494,7 @@ function uxDynamic(data, id, defaults) {
     .on('change', (event) => {
       geog = event.target.value;
       console.log('In the geog_dd, the geog is', geog);
-      renderLines(yCol, dataset, filterVal, colorScale, geog, id);
+      renderLines(xCol, yCol, dataset, filterVal, colorScale, geog, id);
     })
     .selectAll('option')
     .data((dim) => geogs_vars.map((state) => ({state, dim})))
@@ -465,7 +508,7 @@ function uxDynamic(data, id, defaults) {
   buildContainers(id, xLabel);
   // Lines Inside the Containers
   console.log('but the id is here, right?', id);
-  renderLines(yCol, dataset, filterVal, colorScale, geog, id);
+  renderLines(xCol, yCol, dataset, filterVal, colorScale, geog, id);
 }
 
 function buildContainers(id, xLab) {
@@ -474,9 +517,6 @@ function buildContainers(id, xLab) {
     .attr('class', 'chart')
     .attr('height', height)
     .attr('width', width);
-  // .append('g')
-  // .attr('class', 'chartContainer')
-  // .attr('transform', `translate(0, ${margin.top})`);
 
   const axisContainerX = chart
     .append('g')
@@ -489,7 +529,7 @@ function buildContainers(id, xLab) {
     .attr(
       'transform',
       `translate(${plotWidth / 2 + margin.left}, ${
-        plotHeight + margin.top + margin.bottom * 0.6
+        plotHeight + margin.top + margin.bottom * 0.5
       })`,
     )
     .append('text')
@@ -530,9 +570,9 @@ function buildContainers(id, xLab) {
   const legText = legend.append('g').attr('class', 'legText');
 }
 
-function renderLines(variable, dataset, filterVal, colorScale, place, id) {
+function renderLines(xCol, yCol, dataset, filterVal, colorScale, place, id) {
   // const [axisContainerX, axisContainerY, plotContainer, legend] = pieces;
-  const t = transition().duration();
+  const t = transition().duration(1200);
 
   console.log('hi from the render');
   console.log('in the render, the id is', id);
@@ -547,7 +587,7 @@ function renderLines(variable, dataset, filterVal, colorScale, place, id) {
 
   // Domains and Scales
   const xDomain = extent(loc, (d) => d[xCol]);
-  const yDomain = extent(loc, (d) => d[variable]);
+  const yDomain = extent(loc, (d) => d[yCol]);
   console.log(xDomain, yDomain);
 
   const xScale = scaleLinear().domain(xDomain).range([0, plotWidth]);
@@ -555,29 +595,9 @@ function renderLines(variable, dataset, filterVal, colorScale, place, id) {
 
   const lineScale = line()
     .x((d) => xScale(d[xCol]))
-    .y((d) => yScale(d[variable]));
+    .y((d) => yScale(d[yCol]));
 
-  // Build the Plot
-  const lineContainer = plotContainer
-    .selectAll('path')
-    .data(Object.values(cat))
-    .join(
-      (enter) =>
-        enter
-          .append('path')
-          .attr('d', (d) => lineScale(d))
-          .attr('stroke', (d) => colorScale(get_color(d, filterVal))),
-
-      (update) =>
-        update.call((el) =>
-          el
-            .transition(t)
-            .attr('d', (d) => lineScale(d))
-            .attr('stroke', (d) => colorScale(get_color(d, filterVal))),
-        ),
-    )
-    .attr('stroke-width', 2)
-    .attr('fill', 'none');
+  var setup = [{year: 1990, gen_mwh: 0, mwh_pp: 0}];
 
   // Build the Axes and Legend
   chart
@@ -590,8 +610,38 @@ function renderLines(variable, dataset, filterVal, colorScale, place, id) {
   chart
     .select('.axisContainerY')
     .call(axisLeft(yScale).ticks(5).tickSizeOuter(0).tickFormat(format('.2s')));
-  xLab.select('text').text(xLabel);
+  xLab.select('text').text(xLabel).style('opacity', 1);
   createLegend(cat, colorScale, legend);
+
+  // Build the Plot
+  const linecontainer = plotContainer
+    .selectAll('path')
+    .data(Object.values(cat))
+    .join(
+      (enter) =>
+        enter
+          .append('path')
+          .attr('class', 'line')
+          .attr('id', (_, i) => 'line' + i)
+          .attr('d', (d) => lineScale(d))
+          .attr('stroke', (d) => colorScale(get_color(d, filterVal)))
+          .attr('stroke-dasharray', '1000 1000')
+          .attr('stroke-dashoffset', 0),
+
+      (update) =>
+        update.call((el) =>
+          el
+            .attr('d', (d) => lineScale(d))
+            .style('opacity', 1)
+            .attr('stroke', (d) => colorScale(get_color(d, filterVal)))
+            .attr('stroke-dasharray', '1000 1000')
+            .attr('stroke-dashoffset', '1000')
+            .transition(t)
+            .attr('stroke-dashoffset', 0),
+        ),
+    )
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');
 }
 
 //
@@ -644,3 +694,8 @@ function createLegend(dataset, colorScale, legend) {
         ),
     );
 }
+
+//
+//
+// Credit for Line Animation
+// http://bl.ocks.org/fryford/2925ecf70ac9d9b51031
